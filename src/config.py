6be8a6067775dc03b51ffdd95a8d10bcb3b7013e
Dimitri Acosta <dimitri316@gmail.com>
2021-03-29 09:58:49 -0700

    Fix wind default configuration options

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@
 WX_API_KEY = "<YOUR_API_HERE>"
 WX_LOCATION = "<YOUR_DEFAULT_LOCATION>"
 WX_METRIC_TEMP = "<celsius|fahrenheit|kelvin>"
-WX_METRIC_WIND = "<celsius|fahrenheit|kelvin>"
+WX_METRIC_WIND = "<miles_hour|km_hour|knots>"
